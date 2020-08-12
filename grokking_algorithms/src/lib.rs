55c1718c05a6a727de39dfeaea4c874ee7c6dfb7
Brooks Patton <brooks@brookzerker.com>
2020-08-11 21:47:59 -0600

    not sure how I'm liking this

--- grokking_algorithms/src/lib.rs
+++ grokking_algorithms/src/lib.rs
@@ -1,34 +1,65 @@
 use bbggez::{
     ggez::event::EventHandler,
-    ggez::graphics::{Color, DrawMode, Mesh, MeshBuilder},
+    ggez::graphics::{Color, DrawMode, DrawParam, Mesh, MeshBuilder, BLACK},
     ggez::nalgebra::Point2,
-    ggez::{graphics, timer, Context, GameResult},
+    ggez::{graphics, Context, GameResult},
     rand::prelude::*,
 };
 
-const LINES: u8 = 1;
-const NODE_STROKE: f32 = 2.0;
-const NODE_RADIUS: f32 = 5.0;
-
+#[derive(Debug)]
 pub struct GameState {
     node_mesh: Option<Mesh>,
-    rng: ThreadRng,
+    node_locations: [[Point2<f32>; 4]; 4],
+    node_radius: f32,
+    line_size: f32,
+    line_color: Color,
 }
 
 impl GameState {
-    pub fn new() -> GameState {
+    pub fn new(screen_size: (f32, f32)) -> GameState {
+        let standard_space_across = screen_size.0 / 4.0;
+        let standard_space_down = screen_size.1 / 4.0;
+        let mut rng = thread_rng();
+        let mut node_locations = [[Point2::new(0.0, 0.0); 4]; 4];
+        let node_radius = 5.0;
+        let line_color = Color::new(0.0, 1.0, 0.0, 1.0);
+
+        for (y_index, row) in node_locations.iter_mut().enumerate() {
+            row[0].y =
+                (y_index as f32 * standard_space_down) + rng.gen_range(0.0, standard_space_down);
+            row[0].x = -node_radius - 1.0;
+            row[1].y =
+                (y_index as f32 * standard_space_down) + rng.gen_range(0.0, standard_space_down);
+            row[1].x = rng.gen_range(0.0, standard_space_across);
+            row[2].y =
+                (y_index as f32 * standard_space_down) + rng.gen_range(0.0, standard_space_down);
+            row[2].x = (2.0 * standard_space_across) + rng.gen_range(0.0, standard_space_across);
+            row[3].y =
+                (y_index as f32 * standard_space_down) + rng.gen_range(0.0, standard_space_down);
+            row[3].x = screen_size.0 + node_radius + 1.0;
+        }
         GameState {
             node_mesh: None,
-            rng: thread_rng(),
+            node_locations,
+            node_radius,
+            line_size: 1.0,
+            line_color,
         }
     }
 
     fn init(&mut self, context: &mut Context) -> GameResult {
         let node_mesh = MeshBuilder::new()
             .circle(
-                DrawMode::stroke(NODE_STROKE),
+                DrawMode::fill(),
                 Point2::new(0.0, 0.0),
-                NODE_RADIUS,
+                self.node_radius,
+                0.1,
+                BLACK,
+            )
+            .circle(
+                DrawMode::stroke(self.line_size),
+                Point2::new(0.0, 0.0),
+                self.node_radius,
                 0.1,
                 Color::new(0.0, 1.0, 0.0, 1.0),
             )
@@ -41,14 +72,37 @@ impl GameState {
 
 impl EventHandler for GameState {
     fn update(&mut self, context: &mut Context) -> GameResult {
-        if timer::ticks(context) == 1 {
+        if let None = self.node_mesh {
             self.init(context)?;
+        }
+
+        let (screen_width, screen_height) = graphics::drawable_size(context);
+
+        for row in self.node_locations.iter_mut() {
+            for node_location in row.iter_mut() {
+                node_location.x -= 1.0;
+                if node_location.x < 0.0 {
+                    node_location.x = screen_width + self.node_radius + 1.0;
+                }
+            }
         }
         Ok(())
     }
 
     fn draw(&mut self, context: &mut Context) -> GameResult {
         graphics::clear(context, graphics::BLACK);
+
+        if let Some(mesh) = &self.node_mesh {
+            for row in &self.node_locations {
+                let lines = MeshBuilder::new()
+                    .line(row, self.line_size, self.line_color)?
+                    .build(context)?;
+                graphics::draw(context, &lines, DrawParam::new())?;
+                for node in row {
+                    graphics::draw(context, mesh, DrawParam::new().dest(*node))?;
+                }
+            }
+        }
 
         graphics::present(context)
     }
--- grokking_algorithms/src/main.rs
+++ grokking_algorithms/src/main.rs
@@ -3,7 +3,7 @@ const WIDTH: f32 = 1280.0;
 const HEIGHT: f32 = 720.0;
 
 fn main() {
-    let mut game_state = GameState::new();
+    let mut game_state = GameState::new((WIDTH, HEIGHT));
     bbggez::run::run_dim(
         &mut game_state,
         WIDTH,
